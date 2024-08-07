a1626f7750d2bba26df3633b02b3b16c47f97539
SyedAhtzan <211370228@gift.edu.pk>
2024-08-07 22:24:32 +0500

    Create Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Sign_in from './Pages/Sign_in';
+import SignUp from './Pages/SignUp';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>Hello Project</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/SignIn" element={<Sign_in />} />
+        <Route path="/SignUp" element={<SignUp />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/Pages/Sign_in.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sign_in() {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  )
+}
+
+export default Sign_in
