0f35ee2fa36e85c5219ffd47b7a07dd5987e814d
SyedAhtzan <211370228@gift.edu.pk>
2024-10-12 17:57:46 +0500

    full web

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,10 @@ import Profile from './Pages/Profile';
 import Header from './Components/Header';
 import PrivateRoute from './Components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
+import UpdateListing from './Pages/UpdateListing';
+import Listing from './Pages/Listing';
+import Search from './Pages/Search';
+
 
 export default function App() {
   return (
@@ -18,11 +22,14 @@ export default function App() {
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />} >
           <Route path="/Profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path='/update-listing/:listingId' element={<UpdateListing />}/>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
